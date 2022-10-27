efcaa055a5146752197d1945a1509729a4dc22ef
AleksanrKotlyar <kotlyar.alek@gmail.com>
2022-10-27 17:06:40 +0300

    fix add logic

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import ContactForm from './ContactForm';
 import { Filter } from './Filter';
 import { ContactList } from './ContactList';
